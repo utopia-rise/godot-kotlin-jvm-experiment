ba5228837e5235c56698a570563a02f265d7eefb
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-08-14 17:48:19 +0200

    Basic gdnative library

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,24 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+mod godot;
+
+use gdnative::*;
+use std::os::raw::c_void;
+
+#[no_mangle]
+fn godot_gdnative_init(options: *const godot_gdnative_init_options) {
+    godot::init(options)
+}
+
+#[no_mangle]
+fn godot_gdnative_terminate(options: *const godot_gdnative_terminate_options) {
+    godot::terminate(options)
+}
+
+#[no_mangle]
+fn godot_nativescript_init(handle: *const c_void) {
+    godot::nativescript_init(handle)
+}
+
+#[no_mangle]
+fn godot_nativescript_destroy(handle: *const c_void) {
+    godot::nativescript_destroy(handle)
 }
