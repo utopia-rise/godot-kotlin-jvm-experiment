857e30aa9202dc0a6d4a9e5b9c17bc99b4b59956
Cedric Hippmann <cedric.hippmann@hotmail.com>
2020-08-14 21:26:51 +0200

    Assign nativescript pointer

--- src/godot.rs
+++ src/godot.rs
@@ -4,11 +4,13 @@ use std::ffi::c_void;
 use std::ptr::null;
 
 struct GodotState {
-    gdnative: *const godot_gdnative_core_api_struct
+    gdnative: *const godot_gdnative_core_api_struct,
+    nativescript: *const godot_gdnative_ext_nativescript_api_struct
 }
 
 static mut GODOT_STATE: GodotState = GodotState {
-    gdnative: null()
+    gdnative: null(),
+    nativescript: null()
 };
 
 pub struct Godot;
@@ -16,6 +18,16 @@ pub struct Godot;
 pub fn init(options: *const godot_gdnative_init_options) {
     unsafe {
         GODOT_STATE.gdnative = (*options).api_struct;
+        let extension_count = (*GODOT_STATE.gdnative).num_extensions;
+        let extensions = (*GODOT_STATE.gdnative).extensions;
+
+        for i in 0..extension_count {
+            let extension = extensions.offset(i as _);
+            let extension_type = (*(*extension)).type_;
+            if extension_type == 1 {
+                GODOT_STATE.nativescript = extension as *const godot_gdnative_ext_nativescript_api_struct;
+            }
+        }
     }
     println!("GDScript Initialized!")
 }
