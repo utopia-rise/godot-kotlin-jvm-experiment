5b637ec7aef3c2565f19b4be24f751e0d9d376b7
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2020-08-21 19:12:23 +1000

    Delete manual variants

--- godot-loader2/src/native_kvariant.cpp
+++
@@ -1,7 +0,0 @@
-#include "native_kvariant.h"
-
-JClassHelper NativeKVariant::JH = JClassHelper("godot.internal.KVariant");
-
-NativeKVariant::NativeKVariant(jni::JObject wrapped) {
-    this->wrapped = wrapped;
-}
